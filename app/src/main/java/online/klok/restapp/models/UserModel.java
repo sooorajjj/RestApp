30874e97a6d2e5a5c05cfeb2dd54ed4dc10809e8
sooorajjj <sooorajjj@gmail.com>
2016-08-19 10:55:11 +0530

    err .... studio git integration is a mess !

---
+++ app/src/main/java/online/klok/restapp/models/UserModel.java
@@ -0,0 +1,61 @@
+package online.klok.restapp.models;
+
+/**
+ * Created by klok on 18/8/16.
+ */
+public class UserModel {
+
+    /**
+     * id : 1
+     * name : Akash
+     * userId : user_1
+     * created_at : 2016-08-18 05:42:12
+     * updated_at : 2016-08-18 05:42:12
+     */
+
+    private int id;
+    private String name;
+    private String userId;
+    private String created_at;
+    private String updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}
